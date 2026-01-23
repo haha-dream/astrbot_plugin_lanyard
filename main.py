4c9adb1833e45aef9aad4d5ff819c3ea2452a720
haha-dream <Komeiji-Ha@outlook.com>
2026-01-23 17:28:35 +0800

    wtf

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from astrbot.api.star import Context, Star, register
 
 
 @register(
-    "astrbot_plugin_lanyard",
-    "haha-dream",
-    "基于 Lanyard 把你的活动推送到群聊",
-    "v1.0.5",
+    name="astrbot_plugin_lanyard",
+    author="haha-dream",
+    version="v1.0.5",
+    desc="基于 Lanyard 把你的活动推送到群聊",
 )
 class LanyardActivityNotifier(Star):
     """Lanyard 活动推送插件
